fdda984ebb7b3356a474ac773cbd87274f5b41fe
Nayjest <mail@vitaliy.in>
2025-05-19 16:58:54 +0200

    Major refactoring, v0.2.0

--- ai_code_review.py
+++ ai_code_review.py
@@ -1,17 +1,15 @@
 import asyncio
 import tomllib
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from git import Repo
 from unidiff import PatchSet
 from pathlib import Path
-from microcore import storage, configure, prompt, llm_parallel
+import microcore as mc
 
-configure(
+mc.configure(
     DOT_ENV_FILE=Path("~/.env.ai-code-review").expanduser(),  # for usage as local cli
-    PROMPT_TEMPLATES_PATH=(CR_APP_ROOT := Path(__file__).resolve().parent),
-    STORAGE_PATH=CR_APP_ROOT / "storage",
-    EMBEDDING_DB_TYPE="",
+    VALIDATE_CONFIG=False,
     USE_LOGGING=True,
 )
 
@@ -19,34 +17,43 @@ configure(
 @dataclass
 class ProjectCodeReviewConfig:
     prompt: str = ""
+    summary_prompt: str = ""
     report: str = ""
     post_process: str = ""
     retries: int = 3
+    max_code_tokens: int = 32000
+    prompt_vars: dict = field(default_factory=dict)
 
     @staticmethod
     def load():
         fn = ".ai-code-review.toml"
         return ProjectCodeReviewConfig(**{
-            **tomllib.load(open(CR_APP_ROOT / fn, "rb")),
+            **tomllib.load(open(Path(__file__).resolve().parent / fn, "rb")),
             **(tomllib.load(open(fn, "rb")) if Path(fn).exists() else {}),
         })
 
 
 async def main():
-    config = ProjectCodeReviewConfig.load()
+    cfg = ProjectCodeReviewConfig.load()
     repo = Repo('.')
     diff_content = repo.git.diff(repo.remotes.origin.refs.HEAD.reference.name, 'HEAD')
     diff = PatchSet.from_string(diff_content)
-    responses = await llm_parallel(
-        [prompt(config.prompt, input=file) for file in diff],
-        retries=config.retries,
+    responses = await mc.llm_parallel(
+        [mc.prompt(cfg.prompt, input=file, **cfg.prompt_vars) for file in diff],
+        retries=cfg.retries,
         parse_json=True
     )
     issues = {file.path: issues for file, issues in zip(diff, responses) if issues}
-    exec(config.post_process, {**globals(), **locals()})
-    storage.write_json("code_review.json", {"issues": issues})
-    report_text = prompt(config.report, issues=issues)
-    print(report_text)
+    exec(cfg.post_process, {"mc": mc, **locals()})
+    summary = mc.prompt(
+        cfg.summary_prompt,
+        diff=mc.tokenizing.fit_to_token_size(diff, cfg.max_code_tokens),
+        issues=issues,
+        **cfg.prompt_vars
+    ).to_llm() if cfg.summary_prompt else ""
+    report_text = mc.prompt(cfg.report, issues=issues, summary=summary)
+    print(mc.ui.yellow(report_text))
+    open("code-review-report.txt", "w", encoding="utf-8").write(report_text)
 
 
 if __name__ == "__main__":
