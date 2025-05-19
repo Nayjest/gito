a160621706994fceb00092b29b9ac630469e6a97
Nayjest <mail@vitaliy.in>
2025-05-19 17:19:35 +0200

    v0.2.1

--- ai_code_review/cli.py
+++ ai_code_review/cli.py
@@ -9,7 +9,7 @@ import typer
 import microcore as mc
 from git import Repo
 
-from ai_code_review import main
+from .ai_code_review import main
 app = async_typer.AsyncTyper()
 
 if sys.platform == 'win32':
@@ -21,25 +21,17 @@ def cli(ctx: typer.Context):
     if not ctx.invoked_subcommand:
         asyncio.run(main())
 
+
 @app.async_command(help="Configure LLM for local usage interactively")
 async def setup():
     mc.interactive_setup(Path("~/.env.ai-code-review").expanduser())
 
+
 @app.async_command(help="Review remote code")
-async def remote(url = typer.Option(), branch = typer.Option()):
+async def remote(url=typer.Option(), branch=typer.Option()):
     if os.path.exists("reviewed-repo"):
         shutil.rmtree("reviewed-repo")
     Repo.clone_from(url, branch=branch, to_path="reviewed-repo")
     os.chdir("reviewed-repo")
     await main()
     os.chdir("../")
-
-
-
-
-
-
-
-
-
-
