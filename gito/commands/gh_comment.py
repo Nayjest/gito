deb862f53f35012d4d616a03f98904ccce012260
Nayjest <mail@vitaliy.in>
2025-06-19 13:11:48 +0200

    [skip ci] bugfix gito-react-to-comments

--- gito/commands/gh_comment.py
+++ gito/commands/gh_comment.py
@@ -41,11 +41,8 @@ def react_to_comment(
     repo = git.Repo('.')  # Current directory
     owner, repo_name = extract_gh_owner_repo(repo)
     logging.info(f"Using repository: {ui.yellow}{owner}/{repo_name}{ui.reset}")
-    gh_token = gh_token or os.getenv("GIHTUB_TOKEN", None) or os.getenv("GH_TOKEN", None)
+    gh_token = gh_token or os.getenv("GITHUB_TOKEN", None) or os.getenv("GH_TOKEN", None)
     api = GhApi(owner=owner, repo=repo_name, token=gh_token)
-    # temp
-    print('TOK:'+gh_token[:4]+'...')
-    exit()
     comment = api.issues.get_comment(comment_id=comment_id)
     logging.info(
         f"Comment by {ui.yellow('@' + comment.user.login)}: "
