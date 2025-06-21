e650aff51359eda8b6b3983d5ae3d263072c8d17
Nayjest <mail@vitaliy.in>
2025-06-21 02:12:29 +0200

    bugfix

--- gito/constants.py
+++ gito/constants.py
@@ -6,5 +6,5 @@ PROJECT_CONFIG_FILE_PATH = Path(".gito") / PROJECT_CONFIG_FILE_NAME
 PROJECT_CONFIG_BUNDLED_DEFAULTS_FILE = Path(__file__).resolve().parent / PROJECT_CONFIG_FILE_NAME
 HOME_ENV_PATH = Path("~/.gito/.env").expanduser()
 JSON_REPORT_FILE_NAME = "code-review-report.json"
-GITHUB_MD_REPORT_FILE_NAME = "code-review-report.json"
+GITHUB_MD_REPORT_FILE_NAME = "code-review-report.md"
 EXECUTABLE = "gito"
